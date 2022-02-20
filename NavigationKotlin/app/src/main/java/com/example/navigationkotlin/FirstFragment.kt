7526ee70e4245a9e102b7bc45a1021287a0d3250
cosarmuhammet <92250713+cosarmuhammet@users.noreply.github.com>
2022-02-20 12:17:40 +0300

    Navigation

---
+++ NavigationKotlin/app/src/main/java/com/example/navigationkotlin/FirstFragment.kt
@@ -0,0 +1,48 @@
+package com.example.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
+class FirstFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { //tüm görünümler oluştuktan sonra çağrılır
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+
+            val kullaniciSoyAdi = FirstFragmentArgs.fromBundle(it).usersurname
+            textView1.text = kullaniciSoyAdi
+        }
+
+        button1.setOnClickListener {
+
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("Cosar")
+            Navigation.findNavController(it).navigate(action)
+
+        }
+
+
+
+    }
+
+}
